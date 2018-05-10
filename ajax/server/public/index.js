cc3a20f0cba51120f41e33567da15e87f69d1f55
evgeni_d <evgeni_d@optimove.com>
2018-05-10 22:20:24 +0300

    ajax init

---
+++ ajax/server/public/index.js
@@ -0,0 +1,15 @@
+$(function () {
+    let url = 'https://jsonplaceholder.typicode.com/posts';
+    const localUrl = 'http://localhost:3000';
+
+
+
+    /*$.get(url)
+        .then((res)=>{
+            let $ul = $('<ul>');
+            res.map((item)=>{
+                $ul.append($('<li>').text(item.title));
+            });
+            $('.raw-data').append($ul);
+        });*/
+});
